b74695da5e4016fc802a1da36561846645d5b317
miles <miles.pickull@web.de>
2025-11-13 05:19:01 +0100

    fix(contacts-app): increase phoneNumber field length to accommodate longer numbers

--- backend/contacts_app/models.py
+++ backend/contacts_app/models.py
@@ -5,7 +5,7 @@ class Contact(models.Model):
     firstName = models.CharField(max_length=100)
     lastName = models.CharField(max_length=100)
     email = models.EmailField()
-    phoneNumber = models.CharField(max_length=20)  
+    phoneNumber = models.CharField(max_length=40)  
     uid = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
